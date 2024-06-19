a7de14c6a5906105b9db36d4a46524540bf62f53
shireen <shireenk705@gmail.com>
2024-06-19 12:53:08 +0500

    add responsiveness

--- js/index.js
+++
@@ -1,9 +0,0 @@
-      var typed = new Typed("#element", {
-        strings: [
-          '<span class="bg-font blok">Welcome.</span>',
-          '<p>To my <span class="italic lt-purple">Portfolio</span></p>',
-          "<p>I'm <span class='lt-purple'>Syed Mohammad Ammar</span>, a passionate <span class='italic'>front-end developer</span> living in Karachi, Pakistan.</p>",
-        ],
-        typeSpeed: 50,
-        showCursor: false,
-      });
